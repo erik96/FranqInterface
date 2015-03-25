0ff1ba395fe587f327b346f27f48807d99d13e17
Erik Birgiser <ariesrik96@gmail.com>
2015-03-25 19:32:32 +0200

    Expressions: Add sounds

--- app/src/main/java/com/phenom/erik/franqinterface/Fragments/ExpresiiTemplate.java
+++ app/src/main/java/com/phenom/erik/franqinterface/Fragments/ExpresiiTemplate.java
@@ -1,10 +1,13 @@
 package com.phenom.erik.franqinterface.Fragments;
 
 import android.app.Fragment;
+import android.content.Context;
+import android.graphics.Color;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +15,7 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 import com.phenom.erik.franqinterface.R;
+import com.phenom.erik.franqinterface.Util.Constants;
 
 import java.io.File;
 import java.io.IOException;
@@ -19,12 +23,14 @@ import java.io.IOException;
 /**
  * Created by Erik on 3/24/2015.
  */
-public class ExpresiiTemplate extends Fragment implements Button.OnClickListener {
+public class ExpresiiTemplate extends Fragment implements Constants, Button.OnClickListener {
 
     private ImageView imageView;
     private Button button;
     private int resId;
     private String title;
+
+    private Context context;
 
     public void init(int resID, String title) {
         this.resId = resID;
@@ -41,32 +47,45 @@ public class ExpresiiTemplate extends Fragment implements Button.OnClickListener
         button = (Button) view.findViewById(R.id.soundButton);
         button.setOnClickListener(this);
 
+        context = getActivity();
+
         return view;
+    }
+
+    public void playSound() {
+
+        int soundID;
+        int maxN = 7;
+
+        for(int i = 1; i<=maxN; i++) {
+            String name = title + Integer.toString(i);
+            soundID = getResources().getIdentifier(name,"raw",context.getPackageName());
+
+            if(soundID == 0) break;
+
+            MediaPlayer mediaPlayer = MediaPlayer.create(context,soundID);
+            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            try {
+                mediaPlayer.start();
+                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                    public void onCompletion(MediaPlayer mp) {
+                        mp.release();
+
+                    }
+                });
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     @Override
     public void onClick(View v) {
-
-        if(v == button) {
-            String mp3Path = Environment.getExternalStorageDirectory()+"/FranqInterface/audio/" + title + ".mp3";
-
-            if(new File(mp3Path).exists()) {
-                MediaPlayer mediaPlayer = new MediaPlayer();
-                mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
-                try {
-                    mediaPlayer.setDataSource(mp3Path);
-                    mediaPlayer.prepare();
-                    mediaPlayer.start();
-                    mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-                        public void onCompletion(MediaPlayer mp) {
-                            mp.release();
-
-                        }
-                    });
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+        if(v == button) { playSound(); }
     }
 }
